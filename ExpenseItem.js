f86895d744ac4c0255c2b5c6c6f2d9c5d5b57a3b
shivac1998 <97764540+shivac1998@users.noreply.github.com>
2022-08-30 14:44:38 -0700

    Add files via upload

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,13 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
---
+++ Expenses.js
@@ -0,0 +1,52 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  const items = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={items[3].title}
+        amount={items[3].amount}
+        date={items[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
